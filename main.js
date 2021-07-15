fbfc31b9e19b09fb15c61bb466fb23e73ece3f19
andreavaughan <amvaughan16@gmail.com>
2021-07-14 21:57:54 -0400

    Added try catch, started css styles

--- main.js
+++ main.js
@@ -22,59 +22,11 @@ const equate = document.querySelector('.equals')
 
 equate.addEventListener('click', function(event){
     let equation = display.innerText
-    console.log(eval(equation))
-    display.innerText = eval(equation)
+    try {
+        console.log(eval(equation))
+        display.innerText = eval(equation)
+    } catch (SyntaxError) {
+        display.innerText = 'Error'
+    }
 })
 
-
-
-
-
-//################
-// equate.addEventListener('click', function(event){
-//     console.log(event.data)
-// })
-
-// container.addEventListener('click', function(event){
-//     console.log(event.target.dataset.value)
-//     setDisplay(event.target.innerText)
-// })
-
-// num1.addEventListener('click', function (event) {
-//     console.log(event.target.innerText)//Question - why is the console.log important to how this event function runs?
-//     console.log('button clicked')
-//     display.innerText += event.target.innerText
-
-
-
-
-
-
-// const equals = document.getElementById('equals')
-
-// display.addEventListener('input', function(event){
-//     console.log(event.target.innerText)
-//     Selection.toString(display.innerText)
-// })
-
-
-
-
-// equals.addEventListener('click', function(event){
-//     console.log(event.target.innerText)
-//     Selection.toString(display.innerText)
-// })
-
-
-
-// equals.addEventListener('click', function(event){
-//     for 
-// })
-
-// const clear = document.getElementById('clear')
-
-// clear.addEventListener('click', function(event){
-//     console.log(event.target.innerText)
-//     console.log('button clicked')
-//     display.innerText += event.target.innerText
-// })
